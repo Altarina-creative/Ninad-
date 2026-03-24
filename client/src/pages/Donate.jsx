d3c73687d1da03094e3cfbbcc6b96c29206b1341
Altarina-creative <aartirana0030@gmail.com>
2026-03-24 22:33:10 +0530

    Update Donate.jsx

--- client/src/pages/Donate.jsx
+++ client/src/pages/Donate.jsx
@@ -129,10 +129,12 @@ const Donate = () => {
                 <span>State Bank of India</span>
               </div>
 
-              <div className="flex justify-between">
+              <div className="flex flex-col md:flex-row md:justify-between">
                 <span className="font-medium">Branch</span>
-                <span>Mehal Chori District-Chamoli Uttarakhand 246431</span>
-              </div>
+                <span className="break-words md:text-right">
+                   Mehal Chori District-Chamoli Uttarakhand 246431
+                </span>
+                 </div>
 
             </div>
           </div>
