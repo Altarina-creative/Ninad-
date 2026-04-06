0db9201259fdd5b59fd2d01e3f7744c24796613c
Altarina-creative <aartirana0030@gmail.com>
2026-04-06 21:34:07 +0530

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,11 +1,16 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization;
+  let token = req.headers.authorization;
 
   if (!token) return res.status(401).json({ msg: "No token" });
 
   try {
+    // ✅ FIX (Bearer remove)
+    if (token.startsWith("Bearer ")) {
+      token = token.split(" ")[1];
+    }
+
     const verified = jwt.verify(token, "secretkey");
     req.admin = verified;
     next();
