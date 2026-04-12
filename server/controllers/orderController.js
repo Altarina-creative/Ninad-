8c574bbd617de9659b90936d03d0efd76a8ed364
Altarina-creative <aartirana0030@gmail.com>
2026-04-12 14:52:53 +0530

    Update orderController.js

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -8,18 +8,20 @@ const createOrder = async (req, res) => {
     const safeTotal = isNaN(total) ? 0 : total;
 
     // ✅ SAVE ORDER
- const order = new Order({
-  cart: cart.map(item => ({
-    name: item.name,
-    price: Number(item.price),
-    qty: item.qty
-  })),
-  total: safeTotal,
-  email,
-  address,
-  phone,
-  pincode
-});
+    const order = new Order({
+      cart: Array.isArray(cart)
+        ? cart.map(item => ({
+            name: item.name,
+            price: Number(item.price),
+            qty: item.qty
+          }))
+        : [],
+      total: safeTotal,
+      email,
+      address,
+      phone,
+      pincode
+    });
 
     await order.save();
 
