b73549357df202ddc79d640ed3aabdd690ed83e0
Altarina-creative <aartirana0030@gmail.com>
2026-04-12 14:23:10 +0530

    Update orderRoutes.js

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { createOrder } = require("../controllers/orderController");
 
-router.post("/order", createOrder);
+router.post("/", createOrder);
 
 module.exports = router;
